1eb8fa755f7220acc7a9134d8bc9fa06af645409
RNyaupane <roshan.nyaupane@apexcollege.edu.np>
2023-07-25 20:58:47 +0545

    Dark Mode added

--- MY Website/wwwroot/js/scripts.js
+++ MY Website/wwwroot/js/scripts.js
@@ -4,7 +4,6 @@
 // Collapse the navbar by adding the top-nav-collapse class
 window.onscroll = function () {
 	scrollFunction();
-	scrollFunctionBTT(); // back to top button
 };
 
 window.onload = function () {
@@ -70,40 +69,6 @@ if (dropdownCheck !== null) {
 	});
 }
   
-
-/* Card Slider - Swiper */
-//var cardSlider = new Swiper('.card-slider', {
-//	autoplay: {
-//		delay: 4000,
-//		disableOnInteraction: false
-//	},
-//	loop: true,
-//	navigation: {
-//		nextEl: '.swiper-button-next',
-//		prevEl: '.swiper-button-prev'
-//	}
-//});
-
-
-/* Back To Top Button */
-// Get the button
-myButton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-function scrollFunctionBTT() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		myButton.style.display = "block";
-	} else {
-		myButton.style.display = "none";
-	}
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-	document.body.scrollTop = 0; // for Safari
-	document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
-}
-
 
 
 
@@ -290,4 +255,20 @@ function cpuTurn() {
     }
 
     $("#cell" + row + column).click(); // Emulate a click on the cell
+}
+
+
+ //********************************* Dark Theme *****************************
+
+var icon = document.getElementById("ChangeTheme");
+var logo = document.getElementById("logoImage");
+
+icon.onclick = function () {
+    if (document.body.classList.contains("dark-theme")) {
+        logo.src = "/images/logo-roshan-white.png";
+    }
+    else {
+        logo.src = "/images/logo-roshan.png"
+    }
+    
 }
